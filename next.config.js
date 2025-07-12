3b3ed965bd1c53913168bf65a7938c7197889591
Aneep Tandel <aneepct@users.noreply.github.com>
2025-07-12 16:14:23 +0400

    Remove Babel config to fix next/font SWC conflict

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-    esmExternals: false
-  },
-  compiler: {
-    removeConsole: false,
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
