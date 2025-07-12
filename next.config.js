3a399332731a2c974071b40dc6fa2ff207b2a435
Aneep Tandel <aneepct@users.noreply.github.com>
2025-07-12 16:11:15 +0400

    Fix SWC binary loading errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: false,
+  experimental: {
+    esmExternals: false
+  }
+}
 
 module.exports = nextConfig
